1008101e6aae926051bc49038bb5b39ad0e56eaf
micheltanguy <micheltanguy@192.168.0.42>
2013-11-04 12:39:35 +0100

    script initialisation d'un cube

---
+++ PagesPersoMicTanguy/Scripts/cubePicture.js
@@ -0,0 +1,30 @@
+  var renderer, scene, camera, mesh;
+
+ 
+
+  function init(){
+      // on initialise le moteur de rendu
+      renderer = new THREE.WebGLRenderer();
+
+      // si WebGL ne fonctionne pas sur votre navigateur vous pouvez utiliser le moteur de rendu Canvas à la place
+      // renderer = new THREE.CanvasRenderer();
+      renderer.setSize( window.innerWidth, window.innerHeight );
+      document.getElementById('container_3D_animate').appendChild(renderer.domElement);
+
+      // on initialise la scène
+      scene = new THREE.Scene();
+
+      // on initialise la camera que l’on place ensuite sur la scène
+      camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000 );
+      camera.position.set(0, 0, 1000);
+      scene.add(camera);
+      
+      // on créé un  cube au quel on définie un matériau puis on l’ajoute à la scène 
+      var geometry = new THREE.CubeGeometry( 200, 200, 200 );
+      var material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+      mesh = new THREE.Mesh( geometry, material );
+      scene.add( mesh );
+
+      // on effectue le rendu de la scène
+      renderer.render( scene, camera );
+  }
