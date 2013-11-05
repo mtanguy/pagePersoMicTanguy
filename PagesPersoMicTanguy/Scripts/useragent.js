2f53292cf5c8bc489de44dedde3653333b72c0b0
micheltanguy <micheltanguy@192.168.0.42>
2013-11-05 13:28:35 +0100

    ajout d'un  squelette de site mobile dédié avec l'uilistion d'un user agent

---
+++ PagesPersoMicTanguy/Scripts/useragent.js
@@ -0,0 +1,22 @@
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+if( isMobile.any() ) document.location.href = "http://localhost:8888/mobile_website/index.html";
+//if( isMobile.any() ) document.location.href = "http://www.mic.tanguy.free.fr/mobile_website/index.html";
