f885453b284821524513e48b174f2f4999aad215
micheltanguy <micheltanguy@192.168.0.42>
2013-11-12 13:03:10 +0100

    commit and

--- PagesPersoMicTanguy/Scripts/MathsTables.js
+++ PagesPersoMicTanguy/Scripts/MathsTables.js
@@ -13,21 +13,32 @@ function MathsTables(myName,myTerme,myOperateur){
 		 return tHeadStr;
 	 }
 	 function tBody(){
-		 var tBodyStr="";		
-		 for(i=0; i<11; i++){
-		 		
-		 			
-		 			for(j=0; j<11; j++){
-		 			
-		 			if(this.operateur=="+"){opertion=i+j;}
-		 			if(this.operateur=="X"){opertion=i*j;}
+		 var tBodyStr="",i=0,j=0;
+		 if(this.operateur=="+"){operation=i+j;}
+		 if(this.operateur=="X"){operation=i*j;}
+		 if(this.terme1=="terme1"){for(i=0; i<11; i++){
+			 
+		 		for(j=0; j<11; j++){
+		 			 if(this.operateur=="+"){operation=i+j;}
+		 			 if(this.operateur=="X"){operation=i*j;}
 		 			if(i+j==0){operationStr=this.operateur;}
-		 			else {operationStr=i+this.operateur+j+"="+opertion}
+		 			else {operationStr=i+this.operateur+j+"="+operation}
 		 			tBodyStr=tBodyStr+"<td>"+operationStr+"</td>";
 		 			
 		 			}
 		 			tBodyStr="<tr id=table"+i+"_"+j+">"+tBodyStr+"</tr>";	
 		 		}
+		 }else{
+			 for(j=0; j<11; j++){
+	 			 if(this.operateur=="+"){operation=this.terme1+j;}
+	 			 if(this.operateur=="X"){operation=this.terme1*j;}
+	 			if(i+j==0){operationStr=this.operateur;}
+	 			else {operationStr=i+this.operateur+j+"="+operation}
+	 			tBodyStr=tBodyStr+"<td>"+operationStr+"</td>";
+	 			
+	 			}
+	 			tBodyStr="<tr id=table"+i+"_"+j+">"+tBodyStr+"</tr>";
+		 }
 		 
 		 		tBodyStr="<tbody id=tbodyBox"+this.name+">"+tBodyStr+"</tbody>";
 		 		
--- PagesPersoMicTanguy/Scripts/jsScript.js
+++ PagesPersoMicTanguy/Scripts/jsScript.js
@@ -8,7 +8,7 @@ $(function() {
 		//c' est ici que l' on creer le tableau Addition
 		  var tableauAddition =new MathsTables("Tables d\' addition","terme1","+");
 		$("#tableauAddition").html(tableauAddition.tHead(11)+tableauAddition.tBody());
-
+		//c' est ici que l' on creer la table Euclide
 		var tableauEuclide =new MathsTables("Table d\' Euclide","terme1","X");
 		$("#tableauEuclide").html(tableauEuclide.tHead(11)+tableauEuclide.tBody());
 		  
